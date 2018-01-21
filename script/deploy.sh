dfb04d9bea38da8dd71f63a459107982c8ad24a3
Russell Rollins <russell.rollins@salesforce.com>
2018-01-21 15:11:16 -0500

    Adds resume building and CNAME-in' to the script.

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
 # No matter where we call the script from, execute if from the repo root dir.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd "$DIR/.."
@@ -10,8 +18,15 @@ pushd "$DIR/.."
   hugo
 
   echo -e "\033[0;32mLatex resume build...\033[0m"
-  # do stuff
+  pushd resume
+    pdflatex Resume --interaction errorstopmode
+    mv Resume.pdf ../public/assets/resume.pdf
+    rm Resume.log
+    rm Resume.aux
+  popd
 
+  echo -e "\033[0;32mImporting the CNAME file...\033[0m"
+  cp script/CNAME public/CNAME
 
   pushd public
     git add .
