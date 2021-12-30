79f06074ac80e7ec37e96ae2f5f76b3fd1d555c8
Russell Rollins <russell@hashicorp.com>
2021-12-30 12:59:02 -0500

    Still hacking

--- script/deploy.sh
+++ script/deploy.sh
@@ -12,20 +12,20 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 echo -e "\033[0;32mHugo build...\033[0m"
 cd russellrollins
-  hugo --destination="${DIR}/public" --cleanDestinationDir
+  hugo --destination="${DIR}/russellrollins.github.io" --cleanDestinationDir
 cd "$DIR"
 
 
 echo -e "\033[0;32mLatex resume build...\033[0m"
 cd resume
   pdflatex Resume --interaction errorstopmode
-  mkdir -p ../public/assets
-  mv Resume.pdf ../public/assets/resume.pdf
+  mkdir -p "../russellrollins.github.io/assets"
+  mv Resume.pdf ../russellrollins.github.io/assets/resume.pdf
   rm Resume.log
   rm Resume.aux
 cd "$DIR"
 
-cd public
+cd "russellrollins.github.io"
   git add .
   msg="rebuilding site `date`"
   if [ $# -eq 1 ]; then
